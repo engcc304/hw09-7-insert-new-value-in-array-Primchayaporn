db362d7624be352537a91e4e210c5657e27e32a8
Ploy Primchayaporn <137378257+Primchayaporn@users.noreply.github.com>
2023-10-05 13:54:59 +0700

    hw9-7

--- Homework/HW09/hw09-7-insert-new-value-in-array.cpp
+++ Homework/HW09/hw09-7-insert-new-value-in-array.cpp
@@ -16,3 +16,33 @@
         Old Array: 9 2 6 1 7
         New Array: 1 2 2 3 4 4 5 6 7 9
 */
+#include <stdio.h>
+int main () {
+    int N , hold = 0 ;
+    int arr[ 100 ] ;
+    
+    printf( "Enter new element of Array :\n" ) ;
+    scanf( "%d", &N ) ;
+
+    for ( int i = 0 ; i < N ; i++) {
+        printf( "Input :\n" ) ;
+        scanf( "%d",&arr[ i ] ) ;
+    }
+    
+    printf ("Array:" ) ;
+    for (int i = 0; i < N ; i++ ) {
+        printf ( " %d",arr[ i ] ) ;
+    }
+    for (int i = 0 ; i < N ; i++) {
+        for ( int  j = i + 1 ; j < N ; j++) {
+            if ( arr[ i ] < arr [ j ] ) {
+                hold = arr[ i ] ;
+                arr[ i ] = arr[ j ] ;
+                arr[ j ] = hold ;
+            } 
+        }
+    }
+    printf ( "\nThe large 3rd element = %d",arr[ 2 ] ) ;
+    
+    return 0 ;
+}
